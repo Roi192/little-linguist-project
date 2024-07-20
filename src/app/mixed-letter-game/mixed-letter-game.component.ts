b301de57261ea754616d22d83655c811d7fff8cb
Roi192 <roimoshe92@gmail.com>
2024-07-20 23:19:32 +0300

    injuction from service category in mix word game and word sort game

--- src/app/mixed-letter-game/mixed-letter-game.component.ts
+++ src/app/mixed-letter-game/mixed-letter-game.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Category } from '../../shared/model/category';
+import { CategoriesService } from '../services/categories.service';
 
 @Component({
   selector: 'app-mixed-letter-game',
@@ -7,7 +9,13 @@ import { Component, Input } from '@angular/core';
   templateUrl: './mixed-letter-game.component.html',
   styleUrl: './mixed-letter-game.component.css'
 })
-export class MixedLetterGameComponent {
+export class MixedLetterGameComponent implements OnInit {
   @Input () id=""
 
+  
+  currentCategory ?: Category
+constructor(private CategoriesServicy:CategoriesService){}
+ngOnInit(): void {
+  this.currentCategory = this.CategoriesServicy.get(parseInt(this.id))
+}
 }
--- src/app/words-sorter-game/words-sorter-game.component.ts
+++ src/app/words-sorter-game/words-sorter-game.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CategoriesService } from '../services/categories.service';
+import { Category } from '../../shared/model/category';
 
 @Component({
   selector: 'app-words-sorter-game',
@@ -7,7 +9,13 @@ import { Component, Input } from '@angular/core';
   templateUrl: './words-sorter-game.component.html',
   styleUrl: './words-sorter-game.component.css'
 })
-export class WordsSorterGameComponent {
+export class WordsSorterGameComponent implements OnInit {
   @Input () id=""
 
+  currentCategory ?: Category
+constructor(private CategoriesService:CategoriesService){}
+ngOnInit(): void {
+  this.currentCategory = this.CategoriesService.get(parseInt(this.id))
+  
+}
 }
